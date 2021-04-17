af723c55b8bd053bd5150426b88e2404659ba576
Fahad Muntasir <fahad.muntasir@pathao.com>
2021-04-17 16:21:04 +0600

    Manager added for RabbitMQ Support

--- main/main.py
+++ main/main.py
@@ -1,7 +1,27 @@
 from flask import Flask
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UniqueConstraint
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@db/main'
+CORS(app)
 
+db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key = True, autoincrement=False)
+    title = db.Column(db.String(200))
+    image = db.Column(db.String(200))
+
+
+class ProductUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+
+    UniqueConstraint('user_id', 'product_id',  name = 'user_product_unique')
 
 @app.route('/',)
 def index():
